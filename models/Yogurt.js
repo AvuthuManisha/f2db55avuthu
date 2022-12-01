bcd51e024609285acc95b7f6b628da744fb79c72
AvuthuManisha <52159790+AvuthuManisha@users.noreply.github.com>
2022-11-30 19:31:17 -0600

    Added files

--- models/Yogurt.js
+++ models/Yogurt.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const YogurtSchema = mongoose.Schema({
 Yogurt_flavours: String,
-Yogurt_price: Number,
-Yogurt_quantity: Number
+Yogurt_price:{type:Number , min:10},
+Yogurt_quantity: {type:Number , min:100}
 })
 module.exports = mongoose.model("Yogurt",YogurtSchema)
--- routes/Yogurt.js
+++ routes/Yogurt.js
@@ -1,7 +1,13 @@
 var express = require('express');
 const Yogurt_controlers= require('../controllers/Yogurt');
 var router = express.Router();
-
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET Yogurts. */
 router.get('/', Yogurt_controlers.Yogurt_view_all_Page );
 
@@ -10,12 +16,12 @@ router.get('/', Yogurt_controlers.Yogurt_view_all_Page );
 router.get('/detail', Yogurt_controlers.Yogurt_view_one_Page);
 
 /* GET create Yogurt page */
-router.get('/create', Yogurt_controlers.Yogurt_create_Page);
+router.get('/create',secured, Yogurt_controlers.Yogurt_create_Page);
 
 /* GET create update page */
-router.get('/update', Yogurt_controlers.Yogurt_update_Page);
+router.get('/update',secured, Yogurt_controlers.Yogurt_update_Page);
 
 /* GET delete Yogurt page */
-router.get('/delete', Yogurt_controlers.Yogurt_delete_Page);
+router.get('/delete',secured, Yogurt_controlers.Yogurt_delete_Page);
 
 module.exports = router;
